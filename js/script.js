0a95ba04e89aea5c5c9a55c5a3c86e5b3ff178d5
Edvaldo Serra <edvaldoserra44@gmail.com>
2024-11-22 00:17:09 +0000

    Upload do site de portefólio

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// Typing Animation
+
+let letter = 0;
+const text = '    Edvaldo Serra';
+
+function typeWriter() {
+    if(letter < text.length) {
+        document.querySelector(".typing").innerHTML += text.charAt(letter);
+        letter++;
+        setTimeout(typeWriter, 180)
+    }
+}
+
+typeWriter();
+
+
+// Menu Mobile
+
+const navTogglerBtn = document.querySelector(".nav-toggler")
+
+header = document.getElementById("header")
+navTogglerBtn.addEventListener("click", () => {
+    headerSectionTogglerBtn()
+})
+function headerSectionTogglerBtn(){
+    header.classList.toggle("open")
+    navTogglerBtn.classList.toggle("open")
+}
+
+
+// ------
+
+// function redsocial(){
+//     window.alert("[ERRO]! No momento esse botão não está funcionando")
+// }
